9d55806489d1e990f15b618625a0e36df2547e66
朱标 <www.1006801184@qq.com>
2020-09-03 09:46:35 +0800

    no message

--- lib/unitls/them_util.dart
+++ lib/unitls/them_util.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 class ThemUntil {
     static const String THEMSTATE = 'them_state';//主题模式
-
     ThemeData linghtData = ThemeData();  //日间模式
     ThemeData darktData = ThemeData();  //夜间模式
 
